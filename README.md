1659186b82bb9a187cf0e53dd7aa4f0f2e4c271b
soulmachine <soulmachine@gmail.com>
2020-03-18 00:14:13 -0700

    Project skeleton

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # crypto-markets
+
 Get all trading pairs of a cryptocurrency exchange.
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/prefer-default-export
+export function myFunc() {
+  console.info('libary');
+}
