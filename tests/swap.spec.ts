653694ffd678134584e9c993a1b1fad0760e136c
soulmachine <soulmachine@gmail.com>
2020-06-08 20:35:00 -0700

    Added a unit test for Swap market type

---
+++ tests/swap.spec.ts
@@ -0,0 +1,13 @@
+import fetchMarkets from '../src/index';
+
+const SWAP_EXCHANGES = ['Binance', 'BitMEX', 'Bitfinex', 'OKEx', 'Huobi'];
+
+describe('market.pair and market.id should be unique in Swap market', () => {
+  test.each(SWAP_EXCHANGES)('%p', async (exchange) => {
+    const markets = await fetchMarkets(exchange, 'Swap');
+    const dedupPair = new Set(markets.map((m) => m.pair));
+    const dedupId = new Set(markets.map((m) => m.id));
+    expect(markets.length).toEqual(dedupPair.size);
+    expect(markets.length).toEqual(dedupId.size);
+  });
+});
